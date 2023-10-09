f048d55db0c511fb99f0a91c63c2087ccd870b24
Jason A. Wilson <jasonaaronwilson@gmail.com>
2023-10-09 18:07:29 -0400

    the C parser can now parse multiple files read from disk.

--- src/parser/c-to-sexpression.c
+++ src/parser/c-to-sexpression.c
@@ -3,24 +3,28 @@
 #include <stdio.h>
 #include <tree_sitter/api.h>
 
-// Declare the `tree_sitter_json` function, which is
-// implemented by the `tree-sitter-json` library.
+#define C_ARMYKNIFE_LIB_IMPL
+#include "../../../c-armyknife-lib/c-armyknife-lib.h"
+
 TSLanguage *tree_sitter_json();
 
-int main() {
+void process_file(char* file) {
   // Create a parser.
   TSParser *parser = ts_parser_new();
 
   // Set the parser's language (JSON in this case).
   ts_parser_set_language(parser, tree_sitter_c());
 
+  buffer_t* buffer = make_buffer(1024);
+  buffer = buffer_append_file_contents(buffer, file);
+  char* str = buffer_to_c_string(buffer);
+
   // Build a syntax tree based on source code stored in a string.
-  const char *source_code = "void foo() {}";
   TSTree *tree = ts_parser_parse_string(
     parser,
     NULL,
-    source_code,
-    strlen(source_code)
+    str,
+    buffer->length
   );
 
   // Get the root node of the syntax tree.
@@ -31,8 +35,20 @@ int main() {
   printf("%s\n", string);
 
   // Free all of the heap-allocated memory.
+  free(buffer);
+  free(str);
   free(string);
   ts_tree_delete(tree);
   ts_parser_delete(parser);
+}
+
+int main(int argc, char**argv) {
+  command_line_parse_result_t args_and_files = parse_command_line(argc, argv);
+
+  for (int i = 0; i < args_and_files.files->length; i++) {
+    reference_t file = array_get_reference(args_and_files.files, i);
+    process_file(dereference_char_ptr(file));
+  }
+
   return 0;
 }
