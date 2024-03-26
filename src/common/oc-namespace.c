9fe54fb09d1915c35025ed5244e69bcb8f3e758d
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-03-25 20:59:51 -0400

    change namespace to contain arrays intead of hashtables

--- src/common/oc-namespace.c
+++ src/common/oc-namespace.c
@@ -6,16 +6,19 @@
 struct oc_namesspace_S {
   char* name; // Example std, std::io or even the empty string.
 
-  string_hashtable_t* includes;
-  string_hashtable_t* defines;
-  string_hashtable_t* typedefs;
-  string_hashtable_t* enums;
-  string_hashtable_t* structure_forward_declarations;
-  string_hashtable_t* structures;
-  string_hashtable_t* prototypes;
-  string_hashtable_t* inlines;
-  string_hashtable_t* global_variables;
-  string_hashtable_t* functions;
+  // Use arrays (of oc_node_t) so that we perserve the input order,
+  // relieve us of he burden of creating unique keys, and additionally
+  // is more deterministic.
+  value_array_t* includes;
+  value_array_t* defines;
+  value_array_t* typedefs;
+  value_array_t* enums;
+  value_array_t* structure_forward_declarations;
+  value_array_t* structures;
+  value_array_t* prototypes;
+  value_array_t* inlines;
+  value_array_t* global_variables;
+  value_array_t* functions;
 }
 
 #endif /* _OC_NAMESPACE_H_ */
