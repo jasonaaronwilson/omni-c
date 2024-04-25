60ba3e78f30b3baee7c62a575a468b61e4f12d5a
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-04-24 23:20:05 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   Makefile
    	new file:   lexer.c

---
+++ src/lexer.c
@@ -0,0 +1,119 @@
+#line 2 "lexer.c"
+#ifndef _LEXER_H_
+#define _LEXER_H_
+
+#include <c-armyknife-lib.h>
+
+/**
+ * @file lexer
+ *
+ * This is hand-written tokenizer for Omni C (so should work fine for
+ * C and C++).
+ */
+
+/**
+ * @enum token_type
+ *
+ * Represents a broad classification of a Omni C tokens.
+ */
+typedef enum {
+  TOKEN_TYPE_WHITESPACE,
+  TOKEN_TYPE_COMMENT,
+  TOKEN_TYPE_IDENTIFIER,
+  TOKEN_TYPE_PUNCTUATION,
+  TOKEN_TYPE_INTEGER_LITERAL,
+  TOKEN_TYPE_FLOAT_LITERAL,
+  TOKEN_TYPE_STRING_LITERAL,
+  TOKEN_TYPE_CHARACTER_LITERAL
+} token_type_t;
+
+struct oc_token_S {
+  buffer_t* buffer;
+  token_type_t type;
+  int32_t start;
+  int32_t end;
+};
+
+typedef struct oc_token_S oc_token_t;
+
+struct oc_tokenizer_result_S {
+  value_array_t* tokens;
+  int32_t tokenizer_error_code;
+  int32_t tokenizer_error_position;
+};
+
+typedef struct oc_tokenizer_result_S oc_tokenizer_result_t;
+
+char* token_to_string(oc_token_t token);
+
+char* token_type_to_string(token_type_t type);
+
+buffer_t* append_token_debug_string(buffer_t* buffer, oc_token_t token);
+
+oc_tokenizer_result_t tokenize(buffer_t* buffer);
+
+#endif /* _LEXER_H_ */
+
+/**
+ * @function token_to_string
+ *
+ * Return the token as a character string.
+ */
+char* token_to_string(oc_token_t token) {
+  return buffer_c_substring(token.buffer, token.start, token.end);
+}
+
+/**
+ * @function token_type_to_string
+ *
+ * Return a string representation of a token type.
+ */
+char* token_type_to_string(token_type_t type) {
+  switch (type) {
+  case TOKEN_TYPE_WHITESPACE:
+    return "TOKEN_TYPE_WHITESPACE";
+  case TOKEN_TYPE_COMMENT:
+    return "TOKEN_TYPE_COMMENT";
+  case TOKEN_TYPE_IDENTIFIER:
+    return "TOKEN_TYPE_IDENTIFIER";
+  case TOKEN_TYPE_PUNCTUATION:
+    return "TOKEN_TYPE_PUNCTUATION";
+  case TOKEN_TYPE_INTEGER_LITERAL:
+    return "TOKEN_TYPE_INTEGER_LITERAL";
+  case TOKEN_TYPE_FLOAT_LITERAL:
+    return "TOKEN_TYPE_FLOAT_LITERAL";
+  case TOKEN_TYPE_STRING_LITERAL:
+    return "TOKEN_TYPE_STRING_LITERAL";
+  case TOKEN_TYPE_CHARACTER_LITERAL:
+    return "TOKEN_TYPE_CHARACTER_LITERAL";
+  }
+  fatal_error(ERROR_ILLEGAL_STATE);
+}
+
+/**
+ * @function append_token_debug_string
+ *
+ * Append a debug representation of a token to the buffer that looks
+ * roughly like this:
+ *
+ * type: TOKEN_TYPE_IDENTIFIER start: 10 end: 15 str: asdf
+ */
+buffer_t* append_token_debug_string(buffer_t* buffer, oc_token_t token) {
+  char* str = token_to_string(token);
+  buffer = buffer_printf(buffer, "type: %s start: %d end: %d str: %s",
+                         token_type_to_string(token.type), token.start,
+                         token.end, str);
+  free_bytes(str);
+  return buffer;
+}
+
+/**
+ * @function tokenize
+ *
+ * Hopefully return a value array of the tokens but may also return an
+ * error (such as when a character or string literal is unterminated).
+ */
+oc_tokenizer_result_t tokenize(buffer_t* buffer) {
+  oc_tokenizer_result_t result = {0};
+  return result;
+}
