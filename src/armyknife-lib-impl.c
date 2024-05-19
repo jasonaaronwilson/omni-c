4ebce2b9cf4f13f5ce62fec28e6e47ab171ef57b
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-05-19 01:51:20 -0400

    add comment to our "implementation" file that instantiates c-armyknife-lib

--- src/armyknife-lib-impl.c
+++ src/armyknife-lib-impl.c
@@ -1,4 +1,13 @@
 #line 2 "armyknife-lib-impl.c"
 
+/**
+ * C armyknife lib is currently only available as a single source
+ * header file. This just makes sure the parts of the header file that
+ * aren't "inlined" get compiled into our binary. Eventually
+ * c-armyknife-lib will be available as a header plus either ".a" or
+ * ".so" library making this file unnecessary though this isn't really
+ * a pain point by any means or I would have done that already.
+ */
+
 #define C_ARMYKNIFE_LIB_IMPL
 #include <c-armyknife-lib.h>
