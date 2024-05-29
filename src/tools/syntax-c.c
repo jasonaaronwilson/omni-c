20f4207fe3bee9e0bc2544fa8b498783995923ec
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-05-29 09:11:21 -0400

    almost create a tool to test little snippets of C code against gcc, tcc, and clang
    I'm not sure I'm testing the tool correctly. Hopefully by the next
    checkin it will be more obvious...

--- src/tools/syntax-c.c
+++ src/tools/syntax-c.c
@@ -3,27 +3,30 @@
  *
  * A tool that reads stdin and compiles it using gcc, clang and tcc to
  * understand if it is valid as a "top-level" form.
+ *
+ * gcc -o syntax-c yntax-c.c
+ *
+ * echo              | ./syntax-c
+ * echo "int x = 0;" | .a/syntax-c
  */
 
 #define C_ARMYKNIFE_LIB_IMPL
 #include <c-armyknife-lib.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-// jawilson: this appears to be used to construct a command line
-// string. buffer_t* should be pretty good at handling that.
-#define MAX_COMMAND_LINE_SIZE 256
-
-int main() {
-    char filename[] = "temp_src_code.c";
+int main(int argc, char** argv) {
+    char filename[] = "/tmp/syntax-c-temp-src-code.c";
 
     // Slurp stdin
     buffer_t* input_buffer = buffer_append_all(make_buffer(0), stdin);
 
-    // Wrap what we slurped in with a wrapper program...
-    buffer_t* wrapped_src_code = make_buffer(input_buffer->capacity + 1024*1);
+    // Wrap what we slurped in with a wrapper program that is probably
+    // smaller than 1024 bytes (but doesn't have to be...)
+    buffer_t* wrapped_src_code = make_buffer(input_buffer->capacity + 1024);
     wrapped_src_code = buffer_printf(wrapped_src_code, "#include <stdio.h>\nint main() {\n%s\nreturn 0;\n}", 
 				     buffer_to_c_string(input_buffer));
 
@@ -36,18 +39,21 @@ int main() {
     int num_compilers = sizeof(compilers) / sizeof(compilers[0]);
     int num_options = sizeof(options) / sizeof(options[0]);
 
+    int return_code = 1;
     for (int i = 0; i < num_compilers; i++) {
         for (int j = 0; j < num_options; j++) {
-	  char command_line[MAX_COMMAND_LINE_SIZE];
-            snprintf(command_line, sizeof(command_line), "%s %s -o temp_out %s 2> /dev/null", 
-                     compilers[i], options[j], filename);
-            
-            printf("%s compilation with %s: ", compilers[i], options[j]);
-            if (system(command_line) == 0) {
-                printf("SUCCESS\n");
+	  buffer_t* command_line_buffer = buffer_printf(make_buffer(0), 
+						 "%s %s -o temp_out %s 2> /dev/null", compilers[i], options[j], filename);
+	  char* command_line = buffer_to_c_string(command_line_buffer);
+	  if (system(command_line) == 0) {
+	    printf("SUCCESS %s %s\n", compilers[i], options[j]);
+		return_code = 0;
             } else {
-                printf("FAILURE\n");
+	    printf("FAILURE %s %s\n", compilers[i], options[j]);
+		return_code = 1;
             }
+	  free_bytes(command_line);
+	  free_bytes(command_line_buffer);
         }
     }
 
@@ -55,7 +61,7 @@ int main() {
     remove(filename);
     remove("temp_out");
 
-    return 0;
+    return return_code;
 }
 
 /**
