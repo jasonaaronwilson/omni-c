f907b70af1a25717a5c492ed9446a74684dfdb27
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-06-08 17:46:56 -0400

    add command to parse prototypes

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@ boolean_t FLAG_print_tokens_include_whitespace = false;
 boolean_t FLAG_print_tokens_include_comments = false;
 boolean_t FLAG_print_tokens_parse_and_print = true;
 
+char* FLAG_ouput_file = NULL;
+
 void do_print_tokens(value_array_t* tokens, char* message) {
   if (FLAG_print_tokens_show_tokens) {
     // Experimentally, tokens when debug printed are pretty big no
@@ -86,6 +88,53 @@ void print_tokens(void) {
       }
     }
   }
+}
+
+void extract_prototypes(void) {
+  log_info("extract_prototypes()");
+
+  buffer_t* output = make_buffer(16 * 1024);
+  output
+      = buffer_printf(output, "/* Automatically extracted prototypes */\n\n");
+
+  value_array_t* files = read_files(FLAG_files);
+  for (int i = 0; i < FLAG_files->length; i++) {
+    oc_file_t* file = (oc_file_t*) value_array_get(files, i).ptr;
+
+    fprintf(stdout, "====================================================\n");
+    fprintf(stdout, "====> Processing %s\n", file->file_name);
+    fprintf(stdout, "====================================================\n");
+
+    oc_tokenizer_result_t tokenizer_result = tokenize(file->data);
+
+    if (tokenizer_result.tokenizer_error_code) {
+      log_warn("Tokenizer error: \"%s\"::%d -- %d",
+               value_array_get(FLAG_files, i).str,
+               tokenizer_result.tokenizer_error_position,
+               tokenizer_result.tokenizer_error_code);
+      fatal_error(ERROR_ILLEGAL_INPUT);
+    }
+
+    value_array_t* tokens = tokenizer_result.tokens;
+    tokens = transform_tokens(tokens, (token_transformer_options_t){
+                                          .keep_whitespace = false,
+                                          .keep_comments = false,
+                                          .keep_javadoc_comments = false,
+                                          .keep_c_preprocessor_lines = false,
+                                      });
+
+    parse_result_t declarations = parse_declarations(tokens, 0);
+    if (is_error_result(declarations)) {
+      declarations.parse_error.file_name = file->file_name;
+      buffer_t* buffer = make_buffer(1);
+      buffer = buffer_append_human_readable_error(buffer,
+                                                  &(declarations.parse_error));
+      log_fatal("%s", buffer_to_c_string(buffer));
+      fatal_error(ERROR_ILLEGAL_INPUT);
+    }
+  }
+
+  fprintf(stdout, "%s", buffer_to_c_string(output));
 }
 
 //// /**
@@ -288,6 +337,7 @@ void print_tokens(void) {
 ////
 
 void configure_print_tokens_command(void);
+void configure_extract_prototypes_command(void);
 
 void configure_flags() {
   flag_program_name("omni-c");
@@ -299,6 +349,7 @@ void configure_flags() {
   // configure_print_parse_tree_command();
   // configure_extract_header_file_command();
   configure_print_tokens_command();
+  configure_extract_prototypes_command();
 }
 
 //// void configure_build_command(void) {
@@ -323,6 +374,12 @@ void configure_print_tokens_command(void) {
   flag_boolean("--include-whitespace", &FLAG_print_tokens_include_whitespace);
   flag_boolean("--include-comments", &FLAG_print_tokens_include_comments);
   flag_boolean("--parse-and-print", &FLAG_print_tokens_parse_and_print);
+  flag_file_args(&FLAG_files);
+}
+
+void configure_extract_prototypes_command(void) {
+  flag_command("extract-prototypes", &FLAG_command);
+  flag_string("--output-file", &FLAG_ouput_file);
   flag_file_args(&FLAG_files);
 }
 
