c368b67792cc4b35ce3c611146fd1ee298232ecc
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-06-25 02:12:38 -0400

    move extractions functions from main.c to header-file-extractor.c
    Probably not the best name but it might itself get turned into multiple
    files later. I'm still fooling around with using the Nth version the
    develop the Nth version rather than using the N-1th version checked out
    in a different directory or something like that. I'm only doing this
    because I feel like I am learning something in the process but I'll
    probably switch the having a "stable" version soon as that will require
    less mental overhead to get things just right.
    
    One interesting thing about this checkin is that header-file-extractor.c
    doesn't depend on c-single-source-file/generate-header-file. That's
    really only because it is only composed of top-level functions. In case
    it isn't obvious, c-single-source-file/generate-header-file was an
    intermediate tool. It will be interesting to see if anyone if using it
    but by that time, omni-c will do that same thing but better.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 /**
- * This is the main routine for the omni-c transpiler.
+ * @file main.c
+ *
+ * This is the main routine for the omni-c C header-file extractor,
+ * the eventual omni-c language transpiler, and other tools helpful to
+ * C and omni-c users.
  */
 
 #include <stdio.h>
@@ -12,6 +16,7 @@
 #include "token-transformer.h"
 #include <c-armyknife-lib.h>
 
+#include "header-file-extractor.c.generated.h"
 #include "main.c.generated.h"
 
 value_array_t* FLAG_files = NULL;
@@ -97,80 +102,6 @@ void print_tokens(void) {
 //// boolean_t FLAG_prototype_empty_bodies = true;
 
 boolean_t FLAG_unique_prototype_header_files = false;
-
-__attribute__((warn_unused_result)) buffer_t*
-    extract_enums_process_declarations(buffer_t* output,
-                                       declarations_node_t* root) {
-  uint64_t length = node_list_length(root->declarations);
-
-  for (uint64_t i = 0; i < length; i++) {
-    parse_node_t* node = node_list_get(root->declarations, i);
-    if (node->tag == PARSE_NODE_TYPEDEF) {
-      /*
-      output = buffer_append_c_function_node_prototype(output, fn_node);
-      output = buffer_printf(output, "\n");
-      */
-    } else if (node->tag == PARSE_NODE_ENUM) {
-    }
-  }
-
-  return output;
-}
-
-__attribute__((warn_unused_result)) buffer_t*
-    extract_prototypes_process_declarations(buffer_t* output,
-                                            declarations_node_t* root) {
-  uint64_t length = node_list_length(root->declarations);
-
-  for (uint64_t i = 0; i < length; i++) {
-    parse_node_t* node = node_list_get(root->declarations, i);
-    if (node->tag == PARSE_NODE_FUNCTION) {
-      function_node_t* fn_node = to_function_node(node);
-
-      // Skip existing prototypes. We assume multiple prototypes are
-      // allowed by ISO C is long as they are the same. This means
-      // that we can extract prototypes and include them before
-      // existing prototypes and the compiler will help make sure we
-      // did this correctly before actually removing any
-      // prototypes. We could also thus probably emit a prototype
-      // for each prototype and besides having a duplicate (or more)
-      // prototype, everything should technically be OK. Just
-      // ignoring prototypes is certainly useful for debugging.
-      if (fn_node->body == NULL) {
-        continue;
-      }
-
-      // Normally prototypes aren't used for inline functions (and
-      // normally they are declared static...) since the inline
-      // functions often appears instead of their prototypes in a
-      // header file. Honestly I haven't completely figured this out
-      // yet. Skipping these prototypes for now if probably fine for
-      // the demo especially since the wisely used ones will be be
-      // static which we are skipping below anyways (for now).
-      if (fn_node->function_specifier != NULL
-          && token_matches(fn_node->function_specifier, "inline")) {
-        continue;
-      }
-      // "static" for a "top-level" declaration means that the
-      // declaration is not supposed to be visible outside the
-      // compilation unit which sort of corresponds to a single "C"
-      // file if the C pre-processor didn't exist -- or at least
-      // that is where I eventually want to have Omni-C to
-      // behave. At least for now, the simplest thing is to not try
-      // to put them into any header files which we can revisit
-      // later (along with static inline...)
-      if (fn_node->storage_class_specifier != NULL
-          && token_matches(fn_node->function_specifier, "static")) {
-        continue;
-      }
-
-      output = buffer_append_c_function_node_prototype(output, fn_node);
-      output = buffer_printf(output, "\n");
-    }
-  }
-
-  return output;
-}
 
 void extract_command(char* command) {
   log_info("extract_prototypes(%s)", command);
