4ba4961fdd48174e47efc770e11270f1c1f22675
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-07-06 15:54:03 -0400

    more updates to symbol table
    The big issue at first will likely be that we just trust nodes to
    declare the same thing to have the same definition and are not in
    conflict.

--- src/bootstrap-saved/symbol-table.c.generated.h
+++ src/bootstrap-saved/symbol-table.c.generated.h
@@ -4,7 +4,7 @@ symbol_table_map_t* make_symbol_table_map(void);
 
 symbol_table_t* make_symbol_table(void);
 
-void symbol_table_add_binding(symbol_table_map_t* map, symbol_table_binding_t* binding);
+void symbol_table_add_definition_node(symbol_table_map_t* map, char* key_string, parse_node_t* node);
 
 void symbol_table_add_children(symbol_table_t* symbol_table, declarations_node_t* root);
 
--- src/symbol-table.c
+++ src/symbol-table.c
@@ -89,11 +89,21 @@ symbol_table_t* make_symbol_table(void) {
   return result;
 }
 
-void symbol_table_add_binding(symbol_table_map_t* map, symbol_table_binding_t* binding) {
-  if (is_ok(string_ht_find(map->ht, binding->key_string))) {
-    // DUPLICATE!
-    fatal_error(ERROR_ILLEGAL_STATE);
+void symbol_table_add_definition_node(symbol_table_map_t* map, char* key_string, parse_node_t* node) {
+  value_result_t previous_binding = string_ht_find(map->ht, key_string);
+  if (is_ok(previous_binding)) {
+    symbol_table_binding_t* binding = cast(symbol_table_binding_t*, previous_binding.ptr);
+    value_array_add(binding->definition_nodes, ptr_to_value(node));
+    return;
   }
+  symbol_table_binding_t* binding = malloc_struct(symbol_table_binding_t);
+  binding->key_string = key_string;
+  // This is overkill for most things but most allocators will round
+  // up the size to 16 bytes anyways which should easily hold two
+  // values and this of course matches a function prototype plus it's
+  // definition so why not.
+  binding->definition_nodes = make_value_array(2);
+  value_array_add(binding->definition_nodes, ptr_to_value(node));
   map->ht = string_ht_insert(map->ht, binding->key_string, ptr_to_value(binding));
   value_array_add(map->ordered_bindings, ptr_to_value(binding));
 }
@@ -104,7 +114,7 @@ void symbol_table_add_children(symbol_table_t* symbol_table, declarations_node_t
     parse_node_t* node = node_list_get(root->declarations, i);
     switch (node->tag) {
     case PARSE_NODE_ENUM:
-      symbol_table_add_binding(symbol_table->enums, NULL);
+      // symbol_table_add_binding(symbol_table->enums, NULL);
       break;
     case PARSE_NODE_FUNCTION:
     case PARSE_NODE_GLOBAL_VARIABLE_DEFINITION:
