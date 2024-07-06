3a4b866f7c6deaf8f53062bbe8c7657f1949e34d
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-07-06 15:37:30 -0400

    rework symbol table design a little more now that I've had more time to think about it
    I'm sure it will change a little more over time and that's OK. Hopefully
    the interface to the symbol table can remain pretty stable.

--- src/symbol-table.c
+++ src/symbol-table.c
@@ -7,40 +7,58 @@
 /**
  * @struct symbol_table_binding_t
  *
- * These hold multiple pieces of data about a symbol in one the the
- * maps below.
+ * A binding contains multiple pieces of data about a symbol and are
+ * what are stored in symbol_table_maps's which in turn comprise the
+ * symbol table.
  *
- * The next_binding field holds either alternate definitions of the
- * same thing (for example a prototype versus the main function
- * definition), or alernate things with the same name (for example
- * over-loaded functions).
+ * TODO(jawilson): the unrelated_bindings is a strawman design to
+ * allow us to have multiple bindings with the same name that are
+ * logically different such as over-loaded function names. These
+ * logical lists may occasionally become quite large for example
+ * overloaded to_string() functions but it's still better than not
+ * using a symbol table at all and having to search through say ALL
+ * top-level nodes to to determine a match, etc.
  */
 typedef struct symbol_table_binding_S {
+  // Unrelated bindings with the same name. I believe only over-loaded
+  // functions will use unrelated bindings.
+  //
+  // struct symbol_table_binding_S* unrelated_bindings;
+
+  // Typically the shortest name (C identifier) associated with a
+  // binding such as the function name or the name of a structure.
   char* key_string;
-  struct symbol_table_binding_S* next_binding;
-  parse_node_t* definition_node;
+
+  // This contains all related declaration nodes for something like a
+  // function or structure. For example we may have the same prototype
+  // one or more times and the actual full definition of a
+  // function. Currently the order of these is simply how they are
+  // encountered (so the full definition of a function probably comes
+  // after the prototype).
+  value_array_t* definition_nodes;
 } symbol_table_binding_t;
 
 /**
  * @struct symbol_table_map_t
  *
- * A poor man's (insert only) "linked-hash-map".
- *
- * While string_hashtable_t can be iteratated over, this iteration
- * order is not fixed and could lead to more unintentional deltas in
- * generated code so we remember the order at which bindings are added
- * to each map.
+ * A symbol_table_map uses string_hashtable to efficiently find a
+ * binding given it's key string (normally C identifiers) but also
+ * keeps track of the insertion order so that we can deterministically
+ * iterate over the bindings in the symbol_table_map instead of the
+ * "random" order that we would get using string_hashtable_foreach.
  */
 typedef struct symbol_table_map_S {
   string_hashtable_t* ht;
+  // This array may contain NULL instead of a symbol_table_binding
+  // once deletion is supported making it slightly sparse.
   value_array_t* ordered_bindings;
 } symbol_table_map_t;
 
 /**
  * @struct symbol_table_t
  *
- * This contains the symbols for a program divided up by categories
- * for easy traversal.
+ * This contains the symbols for a program divided up by semantic
+ * categories.
  */
 typedef struct symbol_table_S {
   symbol_table_map_t* enums;
