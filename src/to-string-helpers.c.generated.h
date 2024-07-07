f81abd9aed4c3fcec74ee9a47d2da27706f78d7c
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-07-06 23:14:59 -0400

    part two of the previous change. .h and .generated.h aren't supposed to be checked in

--- src/to-string-helpers.c.generated.h
+++
@@ -1,8 +0,0 @@
-/* Automatically extracted prototypes from to-string-helpers.c */
-
-char* parse_node_type_to_string(parse_node_type_t type);
-
-char* parse_error_code_to_string(parse_error_code_t error_code);
-
-char* type_node_kind_to_string(type_node_kind_t kind);
-
