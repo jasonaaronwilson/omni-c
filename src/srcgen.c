03483503b148d8ae61da558f057143e7010fa54d
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-07-30 18:03:30 -0400

    add srcgen.c
    Still needs to generate off of typedefs like header-file-extractor.c.
    
    Slowly we can replace header-file-extractor.c to be based off of symbol
    tables which will make the code a bit cleaner.

---
+++ src/srcgen.c
@@ -0,0 +1,54 @@
+/**
+ * @file srcgen.c
+ *
+ * Given a symbol table, these routines will automatically generate
+ * additional source code such as enum->string and string->enum, etc.
+ */
+
+#ifndef _SRCGEN_H_
+#define _SRCGEN_H_
+
+#endif /* _SRCGEN_H_ */
+
+#include "header-file-printer.h"
+#include "parser.h"
+#include "symbol-table-builder.h"
+#include <c-armyknife-lib.h>
+
+static inline char* remove_type_suffix_1(char* typename) {
+  if (string_ends_with(typename, "_t")) {
+    return string_substring(typename, 0, strlen(typename) - 2);
+  } else {
+    return typename;
+  }
+}
+
+/**
+ * @function srcgen_enum_to_string_converters
+ *
+ * For each enum or typedef'd enum, create two additional routines to
+ * provice conversion between char* and an enum value.
+ *
+ * This works by first generate the routines in textual format and
+ * then using the parser to parse it since it is quite a bit easier to
+ * understand and probably easier to debug.
+ */
+uint64_t srcgen_enum_to_string_converters(symbol_table_t* symbol_table) {
+  buffer_t* buffer = make_buffer(1);
+
+  for (int i = 0; i < symbol_table->enums->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding = value_array_get_ptr(
+        symbol_table->enums->ordered_bindings, i, symbol_table_binding_t*);
+    enum_node_t* enum_node = to_enum_node(
+        value_array_get_ptr(binding->definition_nodes, 0, parse_node_t*));
+    char* enum_node_name = token_to_string(enum_node->name);
+    char* to_string_prefix = remove_type_suffix_1(enum_node_name);
+    char* enum_node_type_string = string_printf("enum %s", enum_node_name);
+    buffer = buffer_append_enum_to_string(buffer, enum_node, to_string_prefix,
+                                          enum_node_type_string);
+    buffer = buffer_append_string_to_enum(buffer, enum_node, to_string_prefix,
+                                          enum_node_type_string);
+    symbol_table_parse_buffer(symbol_table, buffer,
+                              "#<internal-code-generator>");
+  }
+}
