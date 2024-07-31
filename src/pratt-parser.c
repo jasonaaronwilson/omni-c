c7d46e88a20f9192cdddd96af9bcc8cb6964d33b
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-07-31 03:01:35 -0400

    start compiling the pratt expression parser
    still have more to implement and then we'll wire it up to main to test
    it since I'm sure it will be interesting...

--- src/pratt-parser.c
+++ src/pratt-parser.c
@@ -2,6 +2,11 @@
 #ifndef _PRATT_PARSER_H_
 #define _PRATT_PARSER_H_
 
+#include <c-armyknife-lib.h>
+
+#include "parser.h"
+#include "compiler-errors.h"
+
 /**
  * @file pratt-parser.c
  *
@@ -13,21 +18,56 @@
  */
 
 typedef enum {
+  PRATT_PARSE_UNKNOWN,
   PRATT_PARSE_PREFIX_OPERATOR,
   PRATT_PARSE_BINARY_OPERATOR,
   PRATT_PARSE_POSTFIX_OPERATOR,
   PRATT_PARSE_CONDITIONAL,
   PRATT_PARSE_OPEN_PAREN,
+  PRATT_PARSE_IDENTIFIER,
   
 } pratt_parser_operation_t;
+
+typedef enum {
+  LEFT_TO_RIGHT,
+  RIGHT_TO_LEFT,
+} associativity_t;
+
+// clang-format off
+typedef enum {
+  PRECEDENCE_UNKNOWN,        
+  PRECEDENCE_PRIMARY,        // LEFT_TO_RIGHT ---- () [] -> . (literals and identifiers)
+  PRECEDENCE_UNARY,          // RIGHT_TO_LEFT ---- ! ~ ++ -- + - * & (cast) sizeof
+  PRECEDENCE_MULTIPICITIVE,  // LEFT_TO_RIGHT ---- * / %
+  PRECEDENCE_ADDITIVE,       // LEFT_TO_RIGHT ---- + -
+  PRECEDENCE_SHIFT,          // LEFT_TO_RIGHT ---- << >>
+  PRECEDENCE_RELATIONAL,     // LEFT_TO_RIGHT ---- < <= > >=
+  PRECEDENCE_EQUALITY,       // LEFT_TO_RIGHT ---- == !=
+  PRECEDENCE_AND,            // LEFT_TO_RIGHT ---- &
+  PRECEDENCE_XOR,            // LEFT_TO_RIGHT ---- ^
+  PRECEDENCE_OR,             // LEFT_TO_RIGHT ---- |
+  PRECEDENCE_LOGICAL_AND,    // LEFT_TO_RIGHT ---- &&
+  PRECEDENCE_LOGICAL_OR,     // LEFT_TO_RIGHT ---- ||
+  PRECEDENCE_CONDITIONAL,    // RIGHT_TO_LEFT ---- ?:
+  PRECEDENCE_ASSIGNMENT,     // RIGHT_TO_LEFT ---- = += -= *= /= %= &= ^= |= <<= >>=
+  PRECEDENCE_COMMA,          // LEFT_TO_RIGHT ---- ,
+} precedence_t;
+// clang-format on
 
 typedef struct {
   oc_token_t* token;
   pratt_parser_operation_t operation;
-  int precedence;
+  precedence_t precedence;
 } pratt_parser_instruction_t;
 
 #endif /* _PRATT_PARSER_H_ */
+
+pratt_parser_instruction_t get_prefix_instruction(oc_token_t* token);
+pratt_parser_instruction_t get_infix_instruction(oc_token_t* token);
+parse_result_t pratt_handle_instruction(pratt_parser_instruction_t instruction,
+					value_array_t* tokens, 
+					uint64_t position,
+					parse_node_t* left);
 
 /**
  * @function pratt_parse_expression
@@ -37,25 +77,29 @@ typedef struct {
 parse_result_t pratt_parse_expression(value_array_t* tokens, uint64_t position, int precedence) {
   oc_token_t* token = token_at(tokens, position++);
   pratt_parser_instruction_t prefix_instruction = get_prefix_instruction(token);
-  if (prefix == null) {
-    return parse_error_result(PARSE_ERROR_EXPECTED_PREFIX_OPERATOR_OR_TERMINAL, name);
+  if (prefix_instruction.operation == PRATT_PARSE_UNKNOWN) {
+    return parse_error_result(PARSE_ERROR_EXPECTED_PREFIX_OPERATOR_OR_TERMINAL, token);
   }
 
-  parse_result_t left = pratt_handle_instruction(tokens, position, prefix_instruction);
+  parse_result_t left = pratt_handle_instruction(prefix_instruction, tokens, position, NULL);
+  if (!is_valid_result(left)) {
+    return left;
+  }
+  position = left.next_token_position;
 
   while (1) {
-    oc_token_t* infix_token = NULL; // get from parse_result_t
+    oc_token_t* infix_token = token_at(tokens, position);
     pratt_parser_instruction_t infix_instruction = get_infix_instruction(infix_token);
-    if (infix_instruction == NULL) {
+    if (infix_instruction.operation == PRATT_PARSE_UNKNOWN) {
       break;
     }
     if (precedence < infix_instruction.precedence) {
       position++;
-      left = pratt_handle_instruction(infix_instruction, tokens, position, left);
+      left = pratt_handle_instruction(infix_instruction, tokens, position, left.node);
       if (!is_valid_result(left)) {
-	break;
+	return left;
       } else {
-	position = left.ASDF;
+	position = left.next_token_position;
       }
     }
   }
@@ -70,14 +114,14 @@ parse_result_t pratt_parse_expression(value_array_t* tokens, uint64_t position, 
  * since we don't have a unique type for each token though we could do
  * that in the future...)
  */
-pratt_parser_instruction_t get_prefix_instruction(token_t* token) {
-  switch (token->token_type) {
+pratt_parser_instruction_t get_prefix_instruction(oc_token_t* token) {
+  switch (token->type) {
 
   case TOKEN_TYPE_IDENTIFIER:
     return (pratt_parser_instruction_t) {
       .token = token,
       .operation = PRATT_PARSE_IDENTIFIER,
-      .precedence = PRATT_PRECEDENCE_IDENTIFIER,
+      .precedence = PRECEDENCE_PRIMARY,
     };
 
   case TOKEN_TYPE_INTEGER_LITERAL:
@@ -87,7 +131,7 @@ pratt_parser_instruction_t get_prefix_instruction(token_t* token) {
     return (pratt_parser_instruction_t) {
       .token = token,
       .operation = PRATT_PARSE_PREFIX_OPERATOR,
-      .precedence = PRATT_PRECEDENCE_LITERAL,
+      .precedence = PRECEDENCE_PRIMARY,
     };
 
   case TOKEN_TYPE_PUNCTUATION:
@@ -96,8 +140,8 @@ pratt_parser_instruction_t get_prefix_instruction(token_t* token) {
   default:
     return (pratt_parser_instruction_t) {
       .token = token,
-      .operation = PRATT_PARSE_DONE,
-      .precedence = PRATT_PRECEDENCE_DONE,
+      .operation = PRATT_PARSE_UNKNOWN,
+      .precedence = PRECEDENCE_UNKNOWN,
     };
   }
 
@@ -108,7 +152,7 @@ pratt_parser_instruction_t get_prefix_instruction(token_t* token) {
     return (pratt_parser_instruction_t) {
       .token = token,
       .operation = PRATT_PARSE_PREFIX_OPERATOR,
-      .precedence = PRATT_PRECEDENCE_PREFIX_OPERATOR,
+      .precedence = PRECEDENCE_UNARY,
     };
   }
 }
@@ -120,26 +164,22 @@ pratt_parser_instruction_t get_prefix_instruction(token_t* token) {
  * since we don't have a unique type for each token though we could do
  * that in the future...)
  */
-pratt_parser_instruction_t get_infix_instruction(token_t* token) {
-  // TODO(jawilson): assignment...
-  // TODO(jawilson): conditional...
+pratt_parser_instruction_t get_infix_instruction(oc_token_t* token) {
   if (token_matches(token, "+") || token_matches(token, "-")) {
     return (pratt_parser_instruction_t) {
       .token = token,
       .operation = PRATT_PARSE_BINARY_OPERATOR,
-      .precedence = PRATT_PRECEDENCE_SUM,
+      .precedence = PRECEDENCE_ADDITIVE,
     };
   }
   if (token_matches(token, "*") || token_matches(token, "/")) {
     return (pratt_parser_instruction_t) {
       .token = token,
       .operation = PRATT_PARSE_BINARY_OPERATOR,
-      .precedence = PRATT_PRECEDENCE_PRODUCT,
+      .precedence = PRECEDENCE_MULTIPICITIVE,
     };
   }
-  // TODO(jawilson): exponent...
-  // TODO(jawilson): postfix...
-  // TODO(jawilson): call... (aka an open paren)
+  fatal_error(ERROR_ILLEGAL_STATE);
 }
 
 /**
@@ -151,10 +191,10 @@ pratt_parser_instruction_t get_infix_instruction(token_t* token) {
 parse_result_t pratt_handle_instruction(pratt_parser_instruction_t instruction,
 					value_array_t* tokens, 
 					uint64_t position,
-					parse_node_t left) {
+					parse_node_t* left) {
   switch (instruction.operation) {
-  default;
-  break;
+  default:
+    break;
   }
   return parse_result_empty();
 }
