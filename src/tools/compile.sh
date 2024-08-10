8385daf67373af566e2f6e1e982137c7bf57e5ce
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-08-10 18:27:50 -0400

    fix bug in compile.sh since failing tests weren't actually failing
    This showed up when trying to use the statement parser.

--- src/tools/compile.sh
+++ src/tools/compile.sh
@@ -6,5 +6,9 @@ TARGET_C_FILE=/tmp/$(basename "$1")
 shift
 
 $DIR/../omni-c generate-library --output-file=${TARGET_C_FILE} $*
-gcc -I${DIR}/../ ${TARGET_C_FILE}
+if [ $? -ne 0 ] ; then
+  echo "FAIL: generate-library failed to generate ${TARGET_C_FILE}" >&2
+  exit 1
+fi
+exec gcc -I${DIR}/../ ${TARGET_C_FILE}
 
