4ae292959c9b89b1c149c668dfaa504489790d1c
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-08-27 23:10:22 -0400

    make the bisect script a little better
    Just to be clear, this is not "real" bi-section but it did point out the
    file in question pretty quickly. True bisection will probably be a thing
    in the future but unit tests, reusing test-suites, etc., are going to be
    more profitable to get us to a working compiler.

--- src/tools/bisect.sh
+++ src/tools/bisect.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+readonly DIR="$(dirname "${BASH_SOURCE[0]}")"
+readonly ORIGIN=$DIR/../
+
 echo "Running omni-c on each file individually to isolate some failures for debugging"
 
 for file in "$@"; do  # Iterate over all command-line arguments
     echo "Processing $file..."
-    ./omni-c generate-library --dump-symbol-table=false --output-file=/tmp/foo.c "$file"
+    $ORIGIN/omni-c generate-library --dump-symbol-table=false --output-file=/tmp/foo.c "$file"
     if [ $? -ne 0 ]; then
         echo "Error processing $file"
         exit 1 
