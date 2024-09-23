0c4010d2ccef021b866fba2f4554e74bc55808c4
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-09-23 02:46:48 -0400

    when doing a full C compilation compile test write output to /tmp

--- src/tools/compile.sh
+++ src/tools/compile.sh
@@ -3,6 +3,7 @@
 readonly DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 TARGET_C_FILE=/tmp/$(basename "$1")
+TARGET_BINARY=/tmp/$(basename "$1").bin
 shift
 
 $DIR/../build/bin/omni-c generate-library --output-file=${TARGET_C_FILE} --use-statement-parser=false $*
@@ -10,5 +11,5 @@ if [ $? -ne 0 ] ; then
   echo "FAIL: generate-library failed to generate ${TARGET_C_FILE}" >&2
   exit 1
 fi
-exec gcc -I${DIR}/../ ${TARGET_C_FILE}
+exec gcc -o ${TARGET_BINARY} -I${DIR}/../ ${TARGET_C_FILE}
 
