22e01db8c305f869c769a3c8a2c5ef16d1484afc
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-09-24 16:58:31 -0400

    use statement parser for header file and compile tests
    It appears all current tests pass which is a bit surprising (though we
    did write lots of statement and expression tests since the last time we
    tried this).

--- src/tools/compile.sh
+++ src/tools/compile.sh
@@ -6,7 +6,7 @@ TARGET_C_FILE=/tmp/$(basename "$1")
 TARGET_BINARY=/tmp/$(basename "$1").bin
 shift
 
-$DIR/../build/bin/omni-c generate-library --output-file=${TARGET_C_FILE} --use-statement-parser=false $*
+$DIR/../build/bin/omni-c generate-library --output-file=${TARGET_C_FILE} --use-statement-parser=true $*
 if [ $? -ne 0 ] ; then
   echo "FAIL: generate-library failed to generate ${TARGET_C_FILE}" >&2
   exit 1
