14e5681d65f81c262c6537d832e777e6aa13b6d1
Jason A. Wilson <jasonaaronwilson@gmail.com>
2024-11-24 02:02:14 -0500

    in debug output, also print out suffixes for variable declarations
    This is a little better than having it completely hidden. We can
    probably use typeof(int[x]) instead now and not need the suffix kludge.

--- src/debug-printer.c
+++ src/debug-printer.c
@@ -444,6 +444,15 @@ void buffer_append_dbg_variable_definition_node(
     cdl_key(printer, "storage_class_specifier");
     cdl_string(printer, token_to_string(node->storage_class_specifier));
   }
+  if (node->suffixes != NULL) {
+    cdl_key(printer, "suffixes");
+    cdl_start_array(printer);
+    for (uint64_t i = 0; i < node->suffixes->length; i++) {
+      parse_node_t* suffix = value_array_get_ptr(node->suffixes, i, typeof(parse_node_t*));
+      buffer_append_dbg_parse_node(printer, suffix);
+    }
+    cdl_end_array(printer);
+  }
   cdl_end_table(printer);
 }
 
