fb5964c7b10fa23b72649e66cde0974ce5849eda
Jason A. Wilson <jasonaaronwilson@gmail.com>
2025-02-06 06:10:25 -0500

    more lib/ "cleanup"
    Duh, src/global-includes.c isn't part of the lib test target. However,
    we can do some other cleanup.

--- src/lib/boolean.c
+++ src/lib/boolean.c
@@ -1,21 +1,3 @@
-#line 2 "boolean.c"
-/**
- * @file boolean.c
- *
- * Make sure that at least true/false from <stdbool.h> are available
- * and and a new typedef named boolean_t because bool seems ugly. (Use
- * what you prefer!)
- */
-
-// ======================================================================
-// This section is extraced to boolean.h
-// ======================================================================
-
-#ifndef _BOOLEAN_H_
-#define _BOOLEAN_H_
-
-#include <stdbool.h>
-
 /**
  * @type boolean_t
  *
@@ -25,9 +7,7 @@
  * (where only char* is commonly used in this library despite not
  * following the typically naming convention).
  */
+
+#include <stdbool.h>
+
 typedef bool boolean_t;
-
-// #define true ((boolean_t) 1)
-// #define false ((boolean_t) 0)
-
-#endif /* _BOOLEAN_H_ */
