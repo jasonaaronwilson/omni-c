6f6d2ca9e7e03f501f8145e5e969be9671378993
Jason A. Wilson <jasonaaronwilson@gmail.com>
2025-02-28 16:56:54 -0500

    make use_statement_parser default to true
    This will make some command lines simpler in the future.

--- src/main.c
+++ src/main.c
@@ -28,8 +28,7 @@ boolean_t FLAG_generate_enum_convertors = true;
 char* FLAG_expression = nullptr;
 char* FLAG_statement = nullptr;
 boolean_t FLAG_dump_symbol_table = false;
-// TODO(jawilson): make this the default or even remove?
-boolean_t FLAG_use_statement_parser = false;
+boolean_t FLAG_use_statement_parser = true;
 boolean_t FLAG_to_c = false;
 boolean_t FLAG_omit_c_armyknife_include = false;
 char* FLAG_c_compiler = "clang";
