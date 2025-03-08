f6e34d3a568a7189061e3befd6c3f724b932caa0
Jason A. Wilson <jasonaaronwilson@gmail.com>
2025-03-08 06:43:40 -0500

    move generate_c_output_file into it's own file
    Obviously this can be split up more so that it is not "GIGANTIC" (aka
    more than 66 * 2 lines, huge would be say 66 * 1.5, and large is 45-66
    lines of code - source - I made this the flip up).

---
+++ src/generate-c-output-file.c
@@ -0,0 +1,217 @@
+/**
+ * @function generate_c_output_file
+ *
+ * This is effectively the real entery-point of the Omni C to C99
+ * transpiler. It still references FLAG_ variables even though it
+ * isn't just a simple "command".
+ *
+ * At some point I'd like to experiment with NON C output. This
+ * large procedure doesn't help us in that goal.
+ *
+ * TODO(jawilson): use a struct to get rid of FLAG_ usage in this file
+ * by taking in a struct OR better yet, first refactor into
+ * self-standing logical parts and THEN refactor.
+ */
+void generate_c_output_file(boolean_t is_library,
+                            buffer_t* command_line_overview_comment) {
+
+  boolean_t is_header_file = !is_library;
+
+  symbol_table_t* symbol_table = make_symbol_table();
+  parse_and_add_top_level_definitions(symbol_table, FLAG_files,
+                                      FLAG_use_statement_parser);
+  dump_symbol_table("initial parse", symbol_table);
+  if (FLAG_generate_enum_convertors) {
+    srcgen_enum_to_string_converters(symbol_table);
+    dump_symbol_table("enum to string generation", symbol_table);
+  }
+  split_structure_typedefs(symbol_table);
+  dump_symbol_table("split structures", symbol_table);
+  reorder_symbol_table_typedefs(symbol_table);
+  dump_symbol_table("reorder typedefs", symbol_table);
+  reorder_symbol_table_structures(symbol_table);
+  dump_symbol_table("reorder structures", symbol_table);
+
+  buffer_t* buffer = make_buffer(1024 * 8);
+  printer_t* printer = make_printer(buffer, symbol_table, 2);
+
+  add_generated_c_file_header(buffer);
+
+  char* guard_name = "_HEADER_FILE_GUARD_";
+
+  if (is_header_file) {
+    buffer_printf(buffer, "#ifndef %s\n#define %s\n\n", guard_name, guard_name);
+  }
+
+  buffer_append_buffer(buffer, get_reflection_header_buffer());
+
+  boolean_t append_newline_after_system_includes = false;
+  buffer_append_string(buffer, "// ========== system includes ==========\n\n");
+  string_hashtable_t* system_includes_set = make_string_hashtable(19);
+  for (uint64_t i = 0; i < symbol_table->system_includes->length; i++) {
+    append_newline_after_system_includes = true;
+    cpp_include_node_t* node = value_array_get_ptr(
+        symbol_table->system_includes, i, typeof(cpp_include_node_t*));
+    char* include_statement = include_node_to_string(node);
+    if (FLAG_omit_c_armyknife_include
+        && string_starts_with(include_statement, "#include <c-armyknife-lib")) {
+      continue;
+    }
+    if (!is_ok(string_ht_find(system_includes_set, include_statement))) {
+      system_includes_set = string_ht_insert(
+          system_includes_set, include_statement, boolean_to_value(true));
+      buffer_append_string(buffer, include_statement);
+    }
+  }
+  if (append_newline_after_system_includes) {
+    buffer_append_string(buffer, "\n");
+  }
+
+  // result->user_includes
+
+  buffer_append_string(buffer, "// ========== defines ==========\n\n");
+  for (uint64_t i = 0; i < symbol_table->defines->length; i++) {
+    cpp_define_node_t* node = value_array_get_ptr(symbol_table->defines, i,
+                                                  typeof(cpp_define_node_t*));
+    append_cpp_define_node(printer, node);
+    append_string(printer, "\n");
+  }
+
+  buffer_append_string(buffer, "// ========== enums ==========\n\n");
+
+  for (int i = 0; i < symbol_table->enums->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->enums->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    enum_node_t* enum_node = to_enum_node(value_array_get_ptr(
+        binding->definition_nodes, 0, typeof(parse_node_t*)));
+    append_enum_node(printer, enum_node);
+    append_string(printer, ";\n\n");
+  }
+
+  buffer_append_string(buffer, "// ========== typedefs ==========\n\n");
+  for (int i = 0; i < symbol_table->typedefs->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->typedefs->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    typedef_node_t* typedef_node = to_typedef_node(
+        cast(parse_node_t*, value_array_get(binding->definition_nodes, 0).ptr));
+    append_typedef_node(printer, typedef_node);
+    append_string(printer, "\n");
+  }
+
+  buffer_append_string(buffer, "// ========== stuctures/unions ==========\n\n");
+  for (int i = 0; i < symbol_table->structures->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->structures->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    struct_node_t* struct_node = get_full_structure_definition_node(binding);
+    if (struct_node == nullptr) {
+      struct_node = value_array_get_ptr(binding->definition_nodes, 0,
+                                        typeof(struct_node_t*));
+    }
+    append_struct_node(printer, struct_node);
+    append_string(printer, ";\n\n");
+  }
+
+  boolean_t append_newline_after_variables = false;
+  buffer_append_string(buffer, "// ========== global variables ==========\n\n");
+  for (int i = 0; i < symbol_table->variables->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->variables->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    append_variable_definition_node(
+        printer,
+        value_array_get_ptr(binding->definition_nodes, 0,
+                            typeof(variable_definition_node_t*)),
+        is_library);
+    append_string(printer, "\n");
+  }
+
+  if (append_newline_after_variables) {
+    append_string(printer, "\n");
+  }
+
+  boolean_t append_newline_after_prototypes = false;
+  buffer_append_string(buffer,
+                       "// ========== function prototypes ==========\n\n");
+  for (int i = 0; i < symbol_table->functions->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->functions->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    function_node_t* function_node = to_function_node(
+        cast(parse_node_t*, value_array_get(binding->definition_nodes, 0).ptr));
+    if (!is_inlined_function(function_node)) {
+      append_newline_after_prototypes = true;
+      append_c_function_node_prototype(printer, function_node);
+    }
+  }
+  if (append_newline_after_prototypes) {
+    append_string(printer, "\n");
+  }
+
+  boolean_t append_newline_after_inlines = false;
+  buffer_append_string(buffer,
+                       "// ========== inlined functions ==========\n\n");
+  for (int i = 0; i < symbol_table->functions->ordered_bindings->length; i++) {
+    symbol_table_binding_t* binding
+        = value_array_get_ptr(symbol_table->functions->ordered_bindings, i,
+                              typeof(symbol_table_binding_t*));
+    function_node_t* function_node = to_function_node(
+        cast(parse_node_t*, value_array_get(binding->definition_nodes, 0).ptr));
+    if (is_inlined_function(function_node)) {
+      append_newline_after_inlines = true;
+      append_c_function_node_and_body(printer, function_node);
+    }
+  }
+
+  if (append_newline_after_inlines) {
+    buffer_append_string(buffer, "\n");
+  }
+
+  boolean_t append_newline_after_functions = false;
+  if (is_library) {
+    buffer_append_string(buffer, "// ========== functions ==========\n\n");
+    for (int i = 0; i < symbol_table->functions->ordered_bindings->length;
+         i++) {
+      symbol_table_binding_t* binding
+          = value_array_get_ptr(symbol_table->functions->ordered_bindings, i,
+                                typeof(symbol_table_binding_t*));
+      for (int j = 0; j < binding->definition_nodes->length; j++) {
+        function_node_t* function_node = to_function_node(cast(
+            parse_node_t*, value_array_get(binding->definition_nodes, j).ptr));
+        if (!is_inlined_function(function_node)
+            && function_node->body != nullptr) {
+          append_newline_after_functions = true;
+          if (false) {
+            buffer_printf(buffer, "/* i=%d j=%d */\n", i, j);
+          }
+          append_c_function_node_and_body(printer, function_node);
+        }
+      }
+    }
+  }
+  if (append_newline_after_functions) {
+    buffer_append_string(buffer, "\n");
+  }
+
+  if (is_header_file) {
+    buffer_printf(buffer, "\n#endif /* %s */\n", guard_name);
+  }
+
+  buffer_append_buffer(buffer, command_line_overview_comment);
+
+  if (FLAG_c_output_file == nullptr) {
+    fprintf(stdout, "%s\n", buffer_to_c_string(buffer));
+  } else {
+    log_info("Attempting to write buffer to %s", FLAG_c_output_file);
+    // make_writable_if_exists(FLAG_c_output_file);
+    buffer_write_file(buffer, FLAG_c_output_file);
+    // make_file_read_only(FLAG_c_output_file);
+  }
+}
+
+boolean_t is_inlined_function(function_node_t* node) {
+  return token_matches(node->storage_class_specifier, "static")
+         && token_list_contains(node->function_specifiers, "inline");
+}
