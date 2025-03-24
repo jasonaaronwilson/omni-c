e5116698c5860b0f53fabd409559c9368be305d7
Jason A. Wilson <jasonaaronwilson@gmail.com>
2025-03-24 04:39:50 -0400

    convert about half of the pure lib unit tests to go through omni-c like buffer-test.c

--- tests/lib/value-array-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-readonly OMNI_CC_PATH="${BUILD_DIR}/bin/omni-c"
-readonly LIB_PATH="${BUILD_DIR}/bin/lib.oar"
-exec ${OMNI_CC_PATH} test ${LIB_PATH} ${OMNI_C_ROOT}/tests/lib/value-array-test.c
