a81aef1dc87241afa3992eb91bf5c7e32ab9c676
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-03-12 22:18:46 -0400

    Add the start of the roci build "language"

---
+++ src/roci/roci.c
@@ -0,0 +1,100 @@
+/**
+ * @file
+ *
+ * roci is an interpreter for a "nano" Scheme interpreter with an
+ * intentionally minimal runtime. Since it's intended to be used as a
+ * configuration language for a cross platform build tool (rather than
+ * a real Scheme implementation), the tokenizer and parser accept a C
+ * like syntax instead of a traditional s-expression syntax.
+ */
+
+typedef atom_tag_t = enum {
+  ATOM_TAG_UNKNOWN,
+  ATOM_TAG_BOOLEAN,
+  ATOM_TAG_INTEGER,
+  ATOM_TAG_DOUBLE,
+  ATOM_TAG_STRING,
+  // Symbols are not currently interned
+  ATOM_TAG_SYMBOL,
+  ATOM_TAG_CLOSURE,
+  ATOM_TAG_C_PRIMITIVE,
+  ATOM_TAG_PAIR,
+};
+
+/**
+ * @structure atom_t
+ *
+ * Represents a "scheme" atom - either data or a tagged
+ * pointer. Normally a Scheme implementation would try to fit these
+ * into the same size as a pointer by taking advantage of low order
+ * or high order bits of a pointer having predictable values.
+ */
+typedef atom_t = struct {
+  value_t value;
+  atom_tag_t tag;
+};
+
+/**
+ * @structure pair_t
+ *
+ * Pair's are the building blocks of lists in Scheme. These ones are
+ * probably quite a lot bigger than even the most basic
+ * implementations because atom_t is naive.
+ */
+typedef pair_t = struct {
+  atom_t car;
+  atom_t cdr;
+};
+
+/**
+ * @structure env_t
+ *
+ * Represents a typical lexical environment. When parent is null, it
+ * is often called the global environment.
+ */
+typedef env_t = struct {
+  env_t* parent;
+  string_hashtable_t* bindings;  
+};
+
+
+pair_t* atom_to_pair(atom_t atom) {
+  if (atom.tag != ATOM_TAG_PAIR) {
+    // signal an error
+  }
+  return cast(pair_t*, atom.value.ptr);
+}
+
+
+/**
+ * @function eval
+ *
+ * This is obviously the heart of the interpreter.
+ */
+atom_t eval(env_t env, atom_t atom) {
+  if (atom.tag != ATOM_TAG_PAIR) {
+    pair_t* list = atom_to_pair(atom);
+    atom_t car = list->car;
+    if (car.tag == ATOM_TAG_SYMBOL) {
+      char* str = car.value.str;
+      if (string_equal(str, "and")) {
+      }
+      if (string_equal(str, "begin")) {
+      }
+      if (string_equal(str, "define")) {
+      }
+      if (string_equal(str, "if")) {
+      }
+      if (string_equal(str, "lambda")) {
+      }
+      if (string_equal(str, "or")) {
+      }
+      if (string_equal(str, "set!")) {
+      }
+    }
+    // Must be an "application", aka, call to a closure or primitive
+    return atom;
+  }
+  // Everything else is self-evaluating.
+  return atom;
+}
