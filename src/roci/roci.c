5846556c172f430142ae7e30f7c2454931fa2e5d
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-03-21 04:20:17 -0400

    kind of an outline what I am going to do with the roci interpreter

--- src/roci/roci.c
+++ src/roci/roci.c
@@ -1,3 +1,76 @@
+//
+// Explanation of why this file is about this change massively:
+//
+// I've had a change of heart on the direction of this interpreter
+// (this one being so derivative). I've already accepted the "slight"
+// pain of not just using the built in "read" to be both the lexical
+// analyzer and the thing that side-steps the parsing process (only to
+// do that over and over again in the interpreter however cheap
+// compared to parsing, arguably not strictly worse than parsing
+// unless analyzed and measured).
+//
+// I fooled around on paper and I'm simply curious about how this
+// would work. I think we can use a tagged stack, tagged environment,
+// and an untagged heap (since we use a conservative collector).
+// 
+// The idea to the tagged stack is to use two stack pointers, one to
+// 64bit words, and one 1/8 of the size to hold a convenient 8-bit
+// tag. (So two allocations instead of one, they can live anywhere, no
+// spatial locality *required* but really small stacks are possible I
+// guess). Incrementing or decrementing these pointers can be done in
+// parallel and since this is an interpreted virtual machine and real
+// machines are very wide now, there is essentially no real code size
+// bloat (the size of the VM is a constant, dynamicly compile
+// code...). Atomicity is maybe a downside but we have to deal with
+// that in many ways on modern processors now (a conservative
+// collector doesn't care but worth considering as a more general
+// mode). Otherwise squint and pretend we have a 72bit computer! If
+// you *trust* what you are loading from the heap, which you can,
+// because of our strict code generation protocol. The tag stack only
+// puts an extra 1/8 burden on the memory system (aka the TLB).
+//
+// The interesting part to me is actually the "instruction stream",
+// namely what if it was also split that into two as well (we'll call
+// them the opcode stream and an immediate stream). In this case the
+// opcode stream always increments on every virtual cycle but the
+// immediate stream only increments when needed. One goes a byte at a
+// time, the other goes 64bits at a time (in the simplest version
+// without having to do lot of adds for padding, not that big of a
+// deal). If we further virtualize each basic block such that there is
+// no assumed fallthrough (so each bblock ends in a two way branch
+// (simply consuming two immediates), each basic block can have a
+// header.
+//
+// The simplest header is actually just the offset to the immedidate
+// stream (or a pointer to it if we want to waste space and make them
+// 64bits). We have enough information to set *pc* and
+// *immediates-pc*. A single byte might limit the size of a basic
+// block to a few hundred instructions (though always easy to branch
+// to a new basic block, just saying...). Like I said earlier, it
+// could just be a full pointer, just not necessary. ULEB128 is not
+// *super* crazy option.
+//
+// The simplest scheme laid out in memory looks like: header | opcode
+// stream | [?padding (nops!)?] | immediate stream.
+//
+// If the opcode and immediate stream pointers have spatial locality,
+// that is probably going to be good for performance and this is the
+// norm with short basic blocks (loads from nearby locations are are
+// optimized by all modern processors).
+//
+// How dense is this encoding?
+//
+// My first guess is that this encoding is not very dense especially
+// on a 64bit machine because most basic blocks are very short. (Two
+// way branches are less crazy on a 32bit version of this machine.) It
+// will probably be better in strange ways like debugging.
+//
+// We are kind of just getting started: I want to do some funky
+// dynamic dispatch based on the tag stack to find our next "dynamic"
+// target (a hash lookup) and create a sensible but unusual machine
+// that will require further insight.
+// 
+
 /**
  * @file
  *
