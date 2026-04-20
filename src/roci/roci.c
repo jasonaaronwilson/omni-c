26b66c1598f8a28cac158955997499339033fb80
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-04-20 19:20:41 -0400

    roci - implement the underlying roci environment routines

--- src/roci/roci.c
+++ src/roci/roci.c
@@ -78,6 +78,16 @@ typedef roci_vm_state_t = struct {
   roci_bb_t** return_stack;
 };
 
+typedef roci_env_binding_t = struct {
+  value_t value;
+  roci_tag_t tag;
+};
+
+typedef roci_env_t = struct {
+  roci_env_t* parent;
+  string_hashtable_t* bindings;
+};
+
 /**
  * @function
  *
@@ -288,4 +298,34 @@ start_bblock:
     }
   }
   return ROCI_RUNTIME_ERROR_NONE;
+}
+
+roci_env_binding_t* roci_get_var(roci_env_t* env, char* name) {
+  while (env != NULL) {
+    value_result_t find_result = string_ht_find(env->bindings, name);
+    if (is_ok(find_result)) {
+      return cast(roci_env_binding_t*, find_result.ptr);
+    }
+    env = env->parent;
+  }
+  return NULL;
+}
+
+void roci_set_var(roci_env_t* env, char* name, value_t value, roci_tag_t tag) {
+  roci_env_binding_t* binding = roci_get_var(env, name);
+  binding->value = value;
+  binding->tag = tag;
+}
+
+void roci_define_var(roci_env_t* env, char* name, value_t value,
+                     roci_tag_t tag) {
+  value_result_t find_result = string_ht_find(env->bindings, name);
+  if (is_ok(find_result)) {
+    log_fatal("redefinition of a variable %s", name);
+    fatal_error(ERROR_ILLEGAL_STATE);
+  }
+  roci_env_binding_t* binding = malloc_struct(roci_env_binding_t);
+  binding->value = value;
+  binding->tag = tag;
+  string_ht_insert(env->bindings, name, ptr_to_value(binding));
 }
