2d1a337df4afe7704b1f57b9762bb59aa9959172
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-06-05 19:08:01 -0400

    lib - fix terminal test

--- tests/lib/terminal-test.c
+++ tests/lib/terminal-test.c
@@ -5,32 +5,32 @@
 void test_terminal(void) {
   buffer_t* buffer = make_buffer(100);
 
-  buffer = term_clear_screen(buffer);
+   term_clear_screen(buffer);
 
   for (int j = 0; j < 5; j++) {
-    buffer = term_move_cursor_absolute(buffer, 5 + j, 10 + j);
-    buffer = buffer_printf(buffer, "Hello ");
-    buffer = term_set_foreground_color(buffer, 0xff0000);
-    buffer = term_set_background_color(buffer, 0xffffff);
-    buffer = buffer_printf(buffer, "World!\n");
+     term_move_cursor_absolute(buffer, 5 + j, 10 + j);
+     buffer_printf(buffer, "Hello ");
+     term_set_foreground_color(buffer, 0xff0000);
+     term_set_background_color(buffer, 0xffffff);
+     buffer_printf(buffer, "World!\n");
   }
 
-  buffer = term_move_cursor_relative(buffer, -2, -2);
-  buffer = buffer_printf(buffer, "*");
-  buffer = term_move_cursor_relative(buffer, 3, 3);
-  buffer = buffer_printf(buffer, "*");
+   term_move_cursor_relative(buffer, -2, -2);
+   buffer_printf(buffer, "*");
+   term_move_cursor_relative(buffer, 3, 3);
+   buffer_printf(buffer, "*");
 
-  buffer = term_bold(buffer);
-  buffer = buffer_printf(buffer, "[Bold]");
-  buffer = term_reset_formatting(buffer);
+   term_bold(buffer);
+   buffer_printf(buffer, "[Bold]");
+   term_reset_formatting(buffer);
 
-  buffer = term_underline(buffer);
-  buffer = buffer_printf(buffer, "[Underline]");
-  buffer = term_reset_formatting(buffer);
+   term_underline(buffer);
+   buffer_printf(buffer, "[Underline]");
+   term_reset_formatting(buffer);
 
-  buffer = term_italic(buffer);
-  buffer = buffer_printf(buffer, "[Italic]");
-  buffer = term_reset_formatting(buffer);
+   term_italic(buffer);
+   buffer_printf(buffer, "[Italic]");
+   term_reset_formatting(buffer);
 
   box_drawing_t box = {
       .upper_left_corner = 0x250c,
@@ -44,7 +44,7 @@ void test_terminal(void) {
   };
 
   for (int i = 0; i < 20; i++) {
-    buffer = term_draw_box(buffer, 5 + i, 5 + i, 40 + i, 20 + i, &box);
+     term_draw_box(buffer, 5 + i, 5 + i, 40 + i, 20 + i, &box);
   }
 
   printf("%s", buffer_to_c_string(buffer));
