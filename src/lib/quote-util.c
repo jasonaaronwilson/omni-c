a1b426de651a0a826edc4f8e7a2798d209530fa3
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-06-11 16:12:13 -0400

    lib - partially implement string_unquote_c_string

--- src/lib/quote-util.c
+++ src/lib/quote-util.c
@@ -1,5 +1,4 @@
 /**
- *
  * @file quote-util.c
  *
  * A couple of routines to help with quoting and unquoting C strings.
@@ -34,5 +33,46 @@ char* quote_c_string(char* input) {
   }
 
   buffer_append_byte(buf, '"');
+  return buffer_to_c_string(buf);
+}
+
+char* string_unquote_c_string(char* input) {
+  if (input == nullptr) return nullptr;
+
+  int limit = strlen(input) - 1;
+  buffer_t* buf = make_buffer(limit + 10);
+  for (size_t i = 1; i < limit;) {
+    uint8_t c = cast(uint8_t, input[i++]);
+    log_fatal("c = %d\n", c);
+    if (c == '\\') {
+      uint8_t c2 = cast(uint8_t, input[i++]);
+      log_fatal("c2 = %d\n", c2);
+      switch(c2) {
+      case '"':  buffer_append_byte(buf, '\"'); break;
+      case '\\': ; buffer_append_byte(buf, '\\'); break;
+      case 'n': buffer_append_byte(buf, '\n'); break;
+      case 't': buffer_append_byte(buf, '\t'); break;
+      case 'r': buffer_append_byte(buf, '\r'); break;
+      // case 'b': buffer_append_byte(buf, '\\'); buffer_append_byte(buf, 'b');  break;
+      // case 'f': buffer_append_byte(buf, '\\'); buffer_append_byte(buf, 'f');  break;
+      case 'x':
+	buffer_t* hex = make_buffer(3);
+	buffer_append_byte(hex, input[i++]);
+	buffer_append_byte(hex, input[i++]);
+	value_result_t result = string_parse_uint64_hex(buffer_to_c_string(hex));
+	if (!is_ok(result)) {
+	  fatal_error(ERROR_ILLEGAL_INPUT);
+	}
+	buffer_append_byte(buf, result.u64);
+	break;
+    default:
+      fatal_error(ERROR_ILLEGAL_INPUT);
+      break;
+      }
+    } else {
+      buffer_append_byte(buf, c);
+    }
+  }
+
   return buffer_to_c_string(buf);
 }
--- tests/lib/quote-util-test.c
+++ tests/lib/quote-util-test.c
@@ -7,3 +7,13 @@ void test_simple_string_quoting(void) {
   test_assert_string_equal("\"\\n\"", quote_c_string("\n"));
   test_assert_string_equal("\"\\xff\"", quote_c_string("\xff"));
 }
+
+void test_simple_string_unquoting(void) {
+  test_assert_string_equal("Hello World!", string_unquote_c_string("\"Hello World!\""));
+  test_assert_string_equal("\n", string_unquote_c_string("\"\\n\""));
+  test_assert_string_equal("\t", string_unquote_c_string("\"\\t\""));
+  test_assert_string_equal("\r", string_unquote_c_string("\"\\r\""));
+  test_assert_string_equal("\xfe", string_unquote_c_string("\"\\xfe\""));
+}
+
+
