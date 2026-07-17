e7eeb8dc9939c144ac1b38ac0f9540d0a6ade553
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-07-16 23:58:49 -0400

    run-tests.roci is ready to be plugged in
    We'll learn more when we start wiring the tests in.

---
+++ tests/always-failing-test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This test should always FAIL."
+echo "blah blah blah"
+exit 1
---
+++ tests/always-passing-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This test should always pass."
+echo "  You probably shouldn't be seeing this"
+echo "  unless the test runner is printing out"
+echo "  irrelavant stuff"
+
