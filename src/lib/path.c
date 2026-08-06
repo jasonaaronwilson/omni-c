cc4d215835ff6bc0b532e598fee8cc36fa6ebff2
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-08-06 17:01:00 -0400

    add path_is_directory and path_is_file
    We'll probably need a few more.

---
+++ src/lib/path.c
@@ -0,0 +1,15 @@
+boolean_t path_is_directory(const char *path) {
+  struct stat st;
+  if (!path || stat(path, &st) != 0) {
+    return false;
+  }
+  return S_ISDIR(st.st_mode);
+}
+
+boolean_t path_is_file(const char *path) {
+  struct stat st;
+  if (!path || stat(path, &st) != 0) {
+    return false;
+  }
+  return S_ISREG(st.st_mode);
+}
