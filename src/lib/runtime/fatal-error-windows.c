307a0ac34a3f014846b70d3c585e009665a00e6b
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-08-11 18:41:22 -0400

    start a windows port
    We still need to generate a candidate stable binary which we will need
    to do from linux by changing the platform in build.roci and spitting out
    omni-c-stable.c which probably won't compile under linux but that will
    get us started writing configure-windows.bat from windows to get
    ourselves bootsrapped.

---
+++ src/lib/runtime/fatal-error-windows.c
@@ -0,0 +1,7 @@
+char* get_command_line() {
+  return "command line not available under windows right now";
+}
+
+char* get_program_path() {
+  return "<program-path-unknown>";
+}
---
+++ src/lib/runtime/platform-windows.c
@@ -0,0 +1,1 @@
+char* platform(void) { return "windows"; }
