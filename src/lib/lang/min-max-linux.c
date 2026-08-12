78727e9ff3ec378cc0c97a1c36ec061155de325e
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-08-11 23:31:49 -0400

    windows - move min/max to platform specific files
    windows seems to have these predefine (perhaps actually implemented
    correctly...)

---
+++ src/lib/lang/min-max-linux.c
@@ -0,0 +1,25 @@
+/**
+ * @file min-max.c
+ *
+ * Macros version of min and max functions.
+ */
+
+/**
+ * @macro min
+ *
+ * Produce the minimum of a and b. Prior to C23, we evalutate one of
+ * the arguments twice but we eventually hope to evaluate each
+ * argument only once and use the "auto" argument so that the macro
+ * works for any type that can be compared with <.
+ */
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+/**
+ * @macro max
+ *
+ * Produce the maximum of a and b. Prior to C23, we evalutate one of
+ * the arguments twice but we eventually hope to evaluate each
+ * argument only once and use the "auto" argument so that the macro
+ * works for any type that can be compared with >.
+ */
+#define max(a, b) ((a) > (b) ? (a) : (b))
