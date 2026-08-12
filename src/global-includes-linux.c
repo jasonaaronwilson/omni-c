9cd424938e6521a1ea75df6a6ffa4369f6e92ab8
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-08-11 23:02:45 -0400

    windows - remove glob.h from the windows build
    No replacement code yet.

--- src/global-includes-linux.c
+++ src/global-includes-linux.c
@@ -1,2 +1,3 @@
 #include <execinfo.h>
+#include <glob.h>
 #include <unistd.h>
--- src/global-includes.c
+++ src/global-includes.c
@@ -11,7 +11,6 @@
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <glob.h>
 #include <limits.h>
 #include <setjmp.h>
 #include <signal.h>
