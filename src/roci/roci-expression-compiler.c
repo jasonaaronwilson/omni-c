d3c787ec36588521b5e2a25fb197412b67c49f66
Jason A. Wilson <jasonaaronwilson@gmail.com>
2026-09-01 23:01:09 -0400

    roci - incremental progress on recursive decent code generation
    Implementing a few more should allow for testing (maybe we try wiring
    this up to handle just rhs initially since that is easier to slot in).
    
    We obviously also need the primitive functions (which because of their
    funny names, are not going to be easy to clobber - leaving the
    possibilities to become byte codes in the future).

--- src/roci/roci-expression-compiler.c
+++ src/roci/roci-expression-compiler.c
@@ -31,6 +31,7 @@ decent expression parser.
 
 */
 
+/*
 void XXX_roci_compile_statement(roci_compiler_state_t* state) {
   token_t* token = roci_peek_token(state);
   log_info("CURRENT TOKEN IS %s", token_to_string(token));
@@ -48,6 +49,7 @@ void XXX_roci_compile_statement(roci_compiler_state_t* state) {
     roci_compile_call_or_assignment(state);
   }
 }
+*/
 
 typedef assignment_cont_t = enum {
   ASSIGNMENT_CONTINUE_NONE,
@@ -57,28 +59,28 @@ typedef assignment_cont_t = enum {
   ASSIGNMENT_CONTINUE_INDEX_SET,
 };
 
-static inline return_for_assignment(assignment_cont_t cont) {
+static inline boolean_t return_for_assignment(assignment_cont_t cont) {
   return (cont == ASSIGNMENT_CONTINUE_VARIABLE_SET)
     || (cont == ASSIGNMENT_CONTINUE_FIELD_SET)
-    || (cont == ASSIGNMENT_INDEX_SET);
+    || (cont == ASSIGNMENT_CONTINUE_INDEX_SET);
 }
 
 void roci_compile_call_or_assignment_statement(roci_compiler_state_t* state) {
   boolean_t assignment_ok = true;
-  assignment_cont_t cont = roci_compile_expression(state, assignment_ok);
+  assignment_cont_t cont = roci_compile_expression2(state, assignment_ok);
   if (cont == ASSIGNMENT_CONTINUE_NONE) {
     // error. we put something on the stack without anything to
     // consuming it. You won't be able to do "foo() && bar();" like in C...
   } else if (cont == ASSIGNMENT_CONTINUE_DID_CALL) {
     // check for semi colon and return after dropping the result
     finish_call_statement(state);
-  } else if (cont == ASSIGNMENT_CONTINUE_VARIABLE) {
+  } else if (cont == ASSIGNMENT_CONTINUE_VARIABLE_SET) {
     // figure out variable name before recursing on RHS
     finish_variable_assignment_statement(state);
-  } else if (cont == ASSIGNMENT_CONTINUE_FIELD) {
+  } else if (cont == ASSIGNMENT_CONTINUE_FIELD_SET) {
     // parse RHS and do an assignment (in reverse order?)
     finish_field_assignment_statement(state);
-  } else if (cont == ASSIGNMENT_CONTINUE_INDEX) {
+  } else if (cont == ASSIGNMENT_CONTINUE_INDEX_SET) {
     // parse RHS and emit call to operator_index_set function which
     // can either hard-code "methods" on the first argument foo[index]
     // = bar ===> operator_index_set(obj, index, value)
@@ -92,28 +94,51 @@ void roci_compile_call_or_assignment_statement(roci_compiler_state_t* state) {
 ///
 ///
 
-assignment_cont_t roci_compile_expression(roci_compiler_state_t* state, boolean_t assignment_ok) {
+assignment_cont_t roci_compile_expression2(roci_compiler_state_t* state, boolean_t assignment_ok) {
   return roci_compile_logical_or(state, assignment_ok);
 }
 
-assignment_cont_t roci_compile_logical_or(roci_compiler_state_t* state, boolean_t assignment_ok) {n
+assignment_cont_t roci_compile_logical_or(roci_compiler_state_t* state, boolean_t assignment_ok) {
   assignment_cont_t cont = roci_compile_logical_and(state, assignment_ok);
+  return cont;
+  /*
   if (return_for_assignment(cont)) {
     return cont;
   }
-  if (peek() == "||") {
-    assignment_cont_t right_cont = roci_compile_logical_and(state, false);
+  token_t* token = roci_peek_token(state);
+  if (token_matches(token, "||")) {
+    cont = roci_compile_logical_and(state, false);
     if (return_for_assignment(right_cont)) {
-      // ERROR: you can't assign to a boolean, definite a mistake
+      return cont;
     }
-    return 
+    return cont;
   } else {
     return cont;
   }
+  */
+}
+
+assignment_cont_t roci_compile_logical_and(roci_compiler_state_t* state, boolean_t assignment_ok) {
+  assignment_cont_t cont = roci_compile_bitwise_or(state, assignment_ok);
+  return cont;
+  /*
+  if (return_for_assignment(cont)) {
+    return cont;
+  }
+  while (true) {
+    token_t* token = roci_peek_token(state);
+    if (!token_matches(token, "&")) {
+      return cont;
+    }
+    roci_expect_token(state, "&");
+    cont = roci_compile_bitwise_or(state, false);
+    roci_emit_binary_operator(state, "operator_infix_&");
+  }
+  */
 }
 
 assignment_cont_t roci_compile_bitwise_or(roci_compiler_state_t* state, boolean_t assignment_ok) {
-  return roci_parse_bitwise_xor(state, assignment_ok);
+  return roci_compile_bitwise_xor(state, assignment_ok);
 }
 
 assignment_cont_t roci_compile_bitwise_xor(roci_compiler_state_t* state, boolean_t assignment_ok) {
@@ -125,7 +150,20 @@ assignment_cont_t roci_compile_bitwise_and(roci_compiler_state_t* state, boolean
 }
 
 assignment_cont_t roci_compile_equality(roci_compiler_state_t* state, boolean_t assignment_ok) {
-  return roci_compile_relational(state, assignment_ok);
+  assignment_cont_t cont = roci_compile_relational(state, assignment_ok);
+  if (return_for_assignment(cont)) {
+    return cont;
+  }
+  while (true) {
+    token_t* token = roci_peek_token(state);
+    if (!token_matches(token, "==")) {
+      return cont;
+    }
+    roci_expect_token(state, "==");
+    cont = roci_compile_relational(state, false);
+    roci_emit_binary_operator(state, "operator_infix_==");
+  }
+  return cont;
 }
 
 assignment_cont_t roci_compile_relational(roci_compiler_state_t* state, boolean_t assignment_ok) {
@@ -141,7 +179,7 @@ assignment_cont_t roci_compile_multiplicative(roci_compiler_state_t* state, bool
 }
 
 assignment_cont_t roci_compile_unary(roci_compiler_state_t* state, boolean_t assignment_ok) {
-  return roci_compile_unary(state, assignment_ok);
+  return roci_compile_primitive(state, assignment_ok);
 }
 
 assignment_cont_t roci_compile_primitive(roci_compiler_state_t* state, boolean_t assignment_ok) {
@@ -162,6 +200,39 @@ assignment_cont_t roci_compile_postfix(roci_compiler_state_t* state, boolean_t a
   //
   // Otherwise an error.
   return ASSIGNMENT_CONTINUE_NONE;
+}
+
+void roci_emit_binary_operator(roci_compiler_state_t* state, char* name) {
+  roci_emit_get_var(state->current_bb, name);
+
+  roci_bb_builder_t* return_bb = roci_new_bblock(state, "return_bb");
+  buffer_append_byte(state->current_bb->opcodes, ROCI_OPCODE_CALL);
+  value_array_add(state->current_bb->data, i64_to_value(2));
+  value_array_add(state->current_bb->data,
+                  str_to_value(return_bb->bblock_label));
+  state->current_bb = return_bb;
+
+  // roci_emit_debug_info(state, debug_token);
+}
+
+
+void finish_call_statement(roci_compiler_state_t* state) {
+  roci_expect_token(state, ";");
+  roci_emit_opcode(state, ROCI_OPCODE_DROP);
+}
+
+void finish_variable_assignment_statement(roci_compiler_state_t* state) {
+  // figure out variable name before recursing on RHS
+}
+
+void finish_field_assignment_statement(roci_compiler_state_t* state) {
+  // parse RHS and do an assignment (in reverse order?)
+}
+
+void finish_index_assignment_statement(roci_compiler_state_t* state) {
+  // parse RHS and emit call to operator_index_set function which
+  // can either hard-code "methods" on the first argument foo[index]
+  // = bar ===> operator_index_set(obj, index, value)
 }
 
 /*
